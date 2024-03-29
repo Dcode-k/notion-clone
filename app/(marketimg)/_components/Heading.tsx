6cc624432b4ba4d08153ba81771c32ec104eeed9
Dcode-k <Krishnkantpachori50@gmail.com>
2024-03-30 04:37:15 +0530

    changes 29-03-2024

--- app/(marketimg)/_components/Heading.tsx
+++ app/(marketimg)/_components/Heading.tsx
@@ -14,7 +14,7 @@ const {isAuthenticated,isLoading}=useConvexAuth();
     <div className="max-w-3xl space-y-4">
      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
-     </h1>\
+     </h1>
      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Jotion is the connected workspace where <br/>
         better, faster work happens.
@@ -25,17 +25,6 @@ const {isAuthenticated,isLoading}=useConvexAuth();
       </div>
        )}
      {
-      isAuthenticated && !isLoading&&(
-        <Button asChild>
-          <Link href="/documents">
-          Enter Jotion
-          </Link>
-       <ArrowRight className="h-4 w-4 ml-2"/>
-     </Button>
-      )
-     }
-
-     {
       !isAuthenticated && !isLoading &&(
         <SignInButton mode="modal">
           <Button>
@@ -43,6 +32,17 @@ const {isAuthenticated,isLoading}=useConvexAuth();
             <ArrowRight className="h-4 w-4 ml-2"/>
           </Button>
         </SignInButton>
+      )
+     }
+      {
+      isAuthenticated && !isLoading&&(
+        <Button asChild>
+          <Link href="/documents">
+          Enter Jotion
+          <ArrowRight className="h-4 w-4 ml-2"/>
+          </Link>
+     </Button>
+        
       )
      }
      
--- app/(marketimg)/layout.tsx
+++ app/(marketimg)/layout.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import Navbar from './_components/Navbar';
+import Navbar  from "./_components/Navbar";
 
-const MarketingLayout = ({children}:{children:React.ReactNode}) => {
-  return (
-    <div className='h-full dark:bg-[#1f1f1f]'>
-      <Navbar/>
-      <main className='h-full pt-40'>
+const MarketingLayout = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  return ( 
+    <div className="h-full dark:bg-[#1F1F1F]">
+      <Navbar />
+      <main className="h-full pt-40">
         {children}
       </main>
     </div>
-  )
+   );
 }
-
-export default  MarketingLayout;
+ 
+export default MarketingLayout;
