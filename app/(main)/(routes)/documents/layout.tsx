ee98170fd971134400891c55b4fc0a774f76d661
Dcode-k <Krishnkantpachori50@gmail.com>
2024-03-31 01:42:09 +0530

    changes 31-03-2024

---
+++ app/(main)/(routes)/documents/layout.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { Spinner } from '@/components/ui/spinner';
+import { useConvexAuth } from 'convex/react'
+import { redirect } from 'next/navigation';
+import React from 'react'
+import Navigation from '../../_components/navigation';
+
+const MainLayout = ({children}:{children:React.ReactNode}) => {
+    const {isAuthenticated,isLoading}=useConvexAuth();
+    if(isLoading){
+        return(
+            <div className='h-full items-center justify-center'>
+               <Spinner size="lg"/>
+            </div>
+        )
+    }
+
+    if(!isAuthenticated){
+      return  redirect("/");
+    }
+  return (
+    <div className='h-full flex dark:bg-[#1f1f1f]'>
+        <Navigation/>
+        <main className='flex-1 h-full overflow-y-auto'>
+        {children}
+        </main>
+    </div>
+  )
+}
+
+export default MainLayout
---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DocumentsPage = () => {
+  return (
+    <div>
+        This is a protected DocumnetsPage
+    </div>
+  )
+}
+
+export default DocumentsPage
