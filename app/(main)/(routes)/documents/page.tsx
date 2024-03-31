476db57435a32dcd6b2d5a8d90b311ce02e919ea
Dcode-k <Krishnkantpachori50@gmail.com>
2024-04-01 01:11:29 +0530

    changes 01-04-2024

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,37 @@
+"use client"
+import Image from 'next/image'
 import React from 'react'
 
+import { useUser } from '@clerk/clerk-react'
+import { Button } from '@/components/ui/button'
+import { PlusCircle } from 'lucide-react'
+
 const DocumentsPage = () => {
+   const {user}=useUser();
+
   return (
-    <div>
-        This is a protected DocumnetsPage
+    <div className='h-full flex flex-col items-center justify-center space-y-4'>
+        <Image
+        src="/empty.png"
+        alt='Empty'
+        height="300"
+        width="300"
+        className='dark:hidden'
+        />
+        <Image
+        src="/empty-dark.png"
+        alt='Empty'
+        height="300"
+        width="300"
+        className='hidden dark:block'
+        />
+        <h2 className='text-lg font-medium'>
+          Welcome to {user?.firstName}&apos;s Jotion
+        </h2>
+        <Button>
+           <PlusCircle className='h-4 w-4 mr-2'/>
+           Create a note
+        </Button>
     </div>
   )
 }
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,8 +1,9 @@
 import { cn } from '@/lib/utils';
 import { ChevronsLeft, MenuIcon } from 'lucide-react'
 import { usePathname } from 'next/navigation';
-import React, { ElementRef, useRef, useState } from 'react'
+import React, { ElementRef, useEffect, useRef, useState } from 'react'
 import { useMediaQuery } from 'usehooks-ts';
+import UserItem from './user-items';
 
 const Navigation = () => {
     const pathName=usePathname()
@@ -13,6 +14,21 @@ const Navigation = () => {
     const navbarRef=useRef<ElementRef<"div">>(null);
     const [isResetting,setIsResetting]=useState(false);
     const [isCollapsed,setIsCollapsed]=useState(isMobile);
+
+    useEffect(()=>{
+      if(isMobile){
+        collapse();
+      }
+      else{
+        resetWidth();
+      }
+    },[isMobile]);
+
+    useEffect(()=>{
+      if(isMobile){
+        collapse();
+      }
+    },[pathName,isMobile])
    
     const handleMouseDown = (
         event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -45,17 +61,39 @@ const Navigation = () => {
         document.removeEventListener("mouseup", handleMouseUp);
       };
 
+      const resetWidth=()=>{
+        if(sidebarRef.current && navbarRef.current){
+          setIsCollapsed(false);
+          setIsResetting(true);
+
+          sidebarRef.current.style.width=isMobile ? "100%" : "240px";
+          navbarRef.current.style.setProperty("width",isMobile ? "0" : "calc(100% - 240px)");
+          navbarRef.current.style.setProperty("left",isMobile ? "100%" : "240px");
+          setTimeout(()=>setIsResetting(false),300);
+        }
+      }
+
+      const collapse=()=>{
+        if(sidebarRef.current && navbarRef.current){
+          setIsCollapsed(true);
+          setIsResetting(true);
+
+          sidebarRef.current.style.width="0";
+          navbarRef.current.style.setProperty("width","100%");
+          navbarRef.current.style.setProperty("left","0");
+          setTimeout(()=>setIsResetting(false),300)
+        }
+      }
+
   return (
     <>
     <aside ref={sidebarRef} 
     className={cn('group/sidebar  h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]',isResetting && "transition-all ease-in-out duration-300",isMobile && "w-0")}>
-     <div role='button' className={cn('h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100',isMobile && "opacity-100")}>
+     <div  onClick={collapse} role='button' className={cn('h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100',isMobile && "opacity-100")}>
         <ChevronsLeft className='h-6 w-6'/>
      </div>
      <div>
-        <p>
-            Action Items
-        </p>
+        <UserItem/>
      </div>
      <div className='mt-4'>
         <p>
@@ -64,12 +102,12 @@ const Navigation = () => {
      </div>
      <div 
      onMouseDown={handleMouseDown}
-     onClick={()=>{}}
+     onClick={resetWidth}
      className='opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0'/>
     </aside>
     <div  ref={navbarRef} className={cn("absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",isResetting && "transition-all ease-in-out duration-300",isMobile && "left-0 w-full")}>
      <nav className='bg-transparent px-3 py-2 w-full'>
-        {isCollapsed && <MenuIcon role='button' className='h-6 w-6 text-muted-foreground'/>}
+        {isCollapsed && <MenuIcon onClick={resetWidth} role='button' className='h-6 w-6 text-muted-foreground'/>}
      </nav>
     </div>
     </>
