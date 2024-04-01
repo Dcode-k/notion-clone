d3595e8ade702c12ddbcb7b91814a81ad31286b4
Dcode-k <Krishnkantpachori50@gmail.com>
2024-04-02 00:32:49 +0530

    changes 02-04-2024

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,13 +1,20 @@
 import { cn } from '@/lib/utils';
-import { ChevronsLeft, MenuIcon } from 'lucide-react'
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from 'lucide-react'
 import { usePathname } from 'next/navigation';
 import React, { ElementRef, useEffect, useRef, useState } from 'react'
 import { useMediaQuery } from 'usehooks-ts';
 import UserItem from './user-items';
+import { useMutation, useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import Item from './item';
+import { toast } from 'sonner';
 
 const Navigation = () => {
     const pathName=usePathname()
   const isMobile=useMediaQuery("(max-width:768px)")
+
+  const documents=useQuery(api.documents.get);
+  const create=useMutation(api.documents.create);
 
     const isResizingRef=useRef(false);
     const sidebarRef=useRef<ElementRef<"aside">>(null);
@@ -85,6 +92,16 @@ const Navigation = () => {
         }
       }
 
+      const handleCreate=()=>{
+        const promise=create({title:"Untitled"});
+
+        toast.promise(promise,{
+          loading:"Creating a new note...",
+          success:"New note created!",
+          error:"Failed to create a new note."
+        })
+      }
+
   return (
     <>
     <aside ref={sidebarRef} 
@@ -94,11 +111,16 @@ const Navigation = () => {
      </div>
      <div>
         <UserItem/>
+        <Item label='Search' icon={Search} isSearch onClick={()=>{}}/>
+        <Item label='Settings' icon={Settings}  onClick={()=>{}}/>
+        <Item onClick={handleCreate} label="New Page" icon={PlusCircle}/>
      </div>
      <div className='mt-4'>
-        <p>
-         Documents
-        </p>
+        {documents?.map((document)=>(
+          <p key={document._id}>
+            {document.title}
+          </p>
+        ))}
      </div>
      <div 
      onMouseDown={handleMouseDown}
